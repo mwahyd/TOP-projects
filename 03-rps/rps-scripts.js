890378d670dd11a6f1af0b5462176b44bc5363c4
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-03-16 17:23:41 +0500

    allow user to enter choice; validate user choice

--- 03-rps/rps-scripts.js
+++ 03-rps/rps-scripts.js
@@ -7,5 +7,23 @@ function getComputerChoice(array) {
   return array[mappedChars[Math.floor(Math.random() * mappedChars.length)]];
 }
 
+function getPlayerInput(choicesArray) {
+  const choice = prompt("Rock (r), Paper (p), Scissors (s)? ");
+  return validatePlayerInput(choice, choicesArray);
+}
+
+function validatePlayerInput(playerInput, array) {
+  if (Object.keys(array).includes(playerInput)) {
+    return array[playerInput];
+  } else if (Object.values(array).includes(playerInput)) {
+    return playerInput;
+  } else {
+    return "Please enter Rock (r), Paper (p) or Scissors (s)";
+  }
+}
+
 let result = getComputerChoice(choices);
+console.log(result);
+
+result = getPlayerInput(choices);
 console.log(result);
