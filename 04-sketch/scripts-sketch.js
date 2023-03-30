3f16bb3f34e6f8cfc1cf72bd80afcc040061c0bc
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-03-30 16:17:56 +0500

    grab the canvas element; create a container for squares; determine to get canvas width and height dynamically

---
+++ 04-sketch/scripts-sketch.js
@@ -0,0 +1,10 @@
+// query selectors
+const canvas = document.querySelector(".canvas");
+
+// created elements
+const gridContainer = document.createElement("div");
+// const grid = document.createElement("div");
+
+// get canvas dimensions
+console.log(canvas.offsetWidth);
+console.log(canvas.offsetHeight);
