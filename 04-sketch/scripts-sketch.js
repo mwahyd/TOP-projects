3130e0c3d047f8eae050a4d970275ae6f21685da
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-03-30 16:23:49 +0500

    create function to make grid squares; style the square with CSS classes

--- 04-sketch/scripts-sketch.js
+++ 04-sketch/scripts-sketch.js
@@ -8,3 +8,14 @@ const gridContainer = document.createElement("div");
 // get canvas dimensions
 console.log(canvas.offsetWidth);
 console.log(canvas.offsetHeight);
+
+// functions
+function createGrid(squares) {
+  for (const num of Array.from(Array(squares).keys())) {
+    const square = document.createElement("div");
+    square.className = "box";
+    canvas.appendChild(square);
+  }
+}
+
+createGrid(10);
