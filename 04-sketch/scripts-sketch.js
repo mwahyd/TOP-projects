5d3d3b40f1274cd942cacd116d2c8cd739f18769
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-04-03 12:31:20 +0500

    add hover effect to grid squares

--- 04-sketch/scripts-sketch.js
+++ 04-sketch/scripts-sketch.js
@@ -1,6 +1,6 @@
 // vars
-let gridX = 2;
-let gridY = 2;
+let gridX = 16;
+let gridY = 16;
 
 // query selectors
 const canvas = document.querySelector(".canvas");
@@ -9,16 +9,16 @@ const canvas = document.querySelector(".canvas");
 const gridContainer = document.createElement("div");
 // const grid = document.createElement("div");
 
-// get canvas dimensions
-console.log(canvas.offsetWidth);
-console.log(canvas.offsetHeight);
+// // get canvas dimensions
+// console.log(canvas.offsetWidth);
+// console.log(canvas.offsetHeight);
 
 // functions
 function createGrid(width, height) {
   const number = width * height;
   for (const num of Array.from(Array(number).keys())) {
     const square = document.createElement("div");
-    square.classList.add("box-border");
+    square.classList.add("box-border", "square");
 
     // square.style.width = `${canvas.offsetWidth / width - 1}px`;
     // square.style.height = `${canvas.offsetHeight / height - 1}px`;
@@ -28,11 +28,27 @@ function createGrid(width, height) {
     square.style.width = `${dimensionPercent}%`;
     // square.style.height = `${dimensionPercent}%`;
 
-    console.log(dimension);
-    console.log(dimensionPercent);
+    // console.log(dimension);
+    // console.log(dimensionPercent);
 
     canvas.appendChild(square);
   }
 }
 
+// set up grid
 createGrid(gridX, gridY);
+
+function addHover(event) {
+  event.target.classList.add("hover");
+}
+
+function removeHover(event) {
+  event.target.classList.remove("hover");
+}
+
+// event listeners
+const squares = canvas.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("mouseenter", addHover);
+  square.addEventListener("mouseleave", removeHover);
+});
