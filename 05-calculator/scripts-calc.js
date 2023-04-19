5c37b571eb3bf27dd29ee2a09c91d4c2906db5a6
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-04-19 16:00:41 +0500

    display the clicked digits and signs on the calculator; store the digits and signs clicked to numsSigns var

--- 05-calculator/scripts-calc.js
+++ 05-calculator/scripts-calc.js
@@ -3,13 +3,34 @@ const BUTTONS = document.querySelectorAll("button");
 const DISPLAY = document.querySelector(".pressed-buttons");
 
 // vars
+const numsSigns = [0];
 
 // activity functions
+function displayStoreDigits(event) {
+  numsSigns.push(Number(event.target.textContent));
+  DISPLAY.textContent += event.target.textContent;
+  console.log(numsSigns);
+}
+
+function displayStoreSigns(event) {
+  numsSigns.push(event.target.textContent);
+  DISPLAY.textContent += event.target.textContent;
+  console.log(numsSigns);
+}
 
 // handler functions
 function buttonClicked(event) {
-  console.log(event.target);
-  DISPLAY.textContent += event.target.textContent;
+  switch (event.target.className) {
+    case "digit":
+      displayStoreDigits(event);
+      break;
+
+    case "sign":
+      displayStoreSigns(event);
+      break;
+  }
+  //   console.log(event.target);
+  //   DISPLAY.textContent += event.target.textContent;
 }
 
 // event listeners
