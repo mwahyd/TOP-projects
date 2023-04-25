f337de33322d8b1812967ae5233149da9f1259ce
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-04-25 14:16:39 +0500

    create boolean var; update default var values (number1, number2); add updateDigitPressed() func; add updateSignPressed() func; update how digits and signs are handled in buttonClicked() func

--- 05-calc/script-calc.js
+++ 05-calc/script-calc.js
@@ -8,8 +8,9 @@ const buttons = document.querySelectorAll("button");
 // vars
 let total = 0,
   number1 = 0,
+  number2 = 0,
   sign = "",
-  number2 = 0;
+  isSignPressed = false;
 
 // math functions
 function add(num1, num2) {
@@ -42,10 +43,35 @@ function operate(numOne, sign, numTwo) {
 }
 
 // activity functions
+function updateDigitsPressed(event) {
+  if (total === 0) {
+    if (!isSignPressed) {
+      number1 = event.target.textContent;
+    } else if (isSignPressed) {
+      number2 = event.target.textContent;
+    }
+  }
+  console.log(number1);
+  console.log(number2);
+}
+
+function updateSignPressed(event) {
+  isSignPressed = true;
+  sign = event.target.textContent;
+  console.log(sign);
+}
 
 // handler functions
 function buttonClicked(event) {
-  console.log(event.target);
+  //   console.log(event.target.className);
+  switch (event.target.className) {
+    case "digit":
+      updateDigitsPressed(event);
+      break;
+    case "sign":
+      updateSignPressed(event);
+      break;
+  }
 }
 
 // event listeners
