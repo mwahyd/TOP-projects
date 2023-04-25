dd0c48dac90a86b5f3443b043001e254a8f401c6
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-04-25 13:34:58 +0500

    create basic math functions

---
+++ 05-calc/script-calc.js
@@ -0,0 +1,48 @@
+// constants (QS)
+
+// vars
+let total = 0;
+
+// math functions
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// activity functions
+
+// handler functions
+
+// event listeners
+
+let result = add(1, 2);
+console.log(result);
+
+result = add(1, 5);
+console.log(result);
+
+result = subtract(10, 5);
+console.log(result);
+
+result = subtract(1, 5);
+console.log(result);
+
+result = multiply(2, 2);
+console.log(result);
+
+result = divide(5, 2);
+console.log(result);
+
+result = divide(10, 2);
+console.log(result);
