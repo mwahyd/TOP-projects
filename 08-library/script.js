ce4f73b8a3c792ad99af1da051077137c264bd6f
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-07 13:22:19 +0200

    open a modal when plus sign clicked; blur the background; get data from form; store data in array; clear state

--- 08-library/script.js
+++ 08-library/script.js
@@ -34,8 +34,14 @@ function Book(title, author, pages, read) {
 }
 
 // handler functions
-function addBookToLibrary() {
+function addBookToLibrary(obj) {
   // when add is clicked, add created book to digi library array
+  let { title, author, pages, read } = obj;
+  if (pages === "") pages = 0;
+  read === true ? (read = "read") : (read = "not read");
+
+  digiLibrary.push(new Book(title, author, pages, read));
+  console.log(digiLibrary);
 }
 
 function getBooksOnDOM() {
@@ -52,12 +58,70 @@ function createBookCard(book) {
   <p>${book.author}</p>
   <p>${book.pages} pages</p>
   <p class="status">${book.read}</p>
+  <span class="del">&#10006;</span>
   `;
 
   document.querySelector(".content").appendChild(div);
 }
 
+function onFormSubmitValidate(event) {
+  event.preventDefault();
+
+  // submit button
+  event.target[5].disabled = true;
+
+  if (event.target[1].value.trim().toLowerCase() === "") {
+    alert("The 'Title' field is empty");
+    event.target[1].value = "";
+  } else if (event.target[2].value.trim().toLowerCase() === "") {
+    alert("The 'Author' field is empty");
+    event.target[2].value = "";
+  }
+
+  const fields = {
+    title: event.target[1].value.trim().toLowerCase(),
+    author: event.target[2].value.trim().toLowerCase(),
+    pages: event.target[3].value,
+    read: event.target[4].checked,
+  };
+
+  addBookToLibrary(fields);
+  clearState(event.target);
+}
+
 // action functions
+function clearState(array) {
+  if (array) {
+    Array.from(array).forEach((item) => {
+      if (item.type === "checkbox") {
+        item.checked = false;
+      } else if (item.type === "submit") {
+      } else {
+        item.value = "";
+      }
+    });
+  }
+  document.querySelector(".modal").classList.add("hidden");
+  document.querySelector(".blur").classList.add("hidden");
+  document.querySelector("#add").disabled = false;
+
+  // get books from storage
+  getBooksOnDOM();
+}
 
 // event listeners
 document.addEventListener("DOMContentLoaded", getBooksOnDOM);
+
+document.querySelector(".add-btn").addEventListener("click", (event) => {
+  document.querySelector(".modal").classList.remove("hidden");
+  document.querySelector(".blur").classList.remove("hidden");
+});
+
+document.querySelector(".close-btn").addEventListener("click", (event) => {
+  document.querySelector(".modal").classList.add("hidden");
+  document.querySelector(".blur").classList.add("hidden");
+});
+
+document
+  .querySelector(".modal")
+  .addEventListener("submit", onFormSubmitValidate);
