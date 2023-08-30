e139bff3438d04ca6fbe3ae30a2930eb44edde0e
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-30 15:09:38 +0200

    cache the main container and player1/2 buttons; bind a click event to the buttons to show the form; create reset function

--- 09-tic-tac-toe/01-object-literal-pattern/script.js
+++ 09-tic-tac-toe/01-object-literal-pattern/script.js
@@ -9,12 +9,35 @@ const game = {
   },
 
   cacheMenuDOM: function () {
-    this.main = document.querySelector("#main");
+    this.container = document.querySelector(".container");
+    this.player1Btn = this.container.querySelector(".player1-btn");
+    // this.player1Form = this.container.querySelector(".player1-form");
+
+    this.player2Btn = this.container.querySelector(".player2-btn");
+    // this.player2Form = this.container.querySelector(".player2-form");
   },
 
-  bindEvents: function () {},
+  bindEvents: function () {
+    this.player1Btn.addEventListener("click", this.showPlayerCard.bind(this));
+    this.player2Btn.addEventListener("click", this.showPlayerCard.bind(this));
+  },
 
   // handler functions
+  showPlayerCard: function (event) {
+    if (event.target.classList.contains("player2-btn")) {
+      event.target.nextElementSibling.classList.add("hidden");
+      // event.target.parentElement.classList.add(".card-border-rad");
+    }
+    event.target.classList.add("hidden");
+    event.target.parentElement.firstElementChild.classList.remove("hidden");
+    event.target.parentElement.classList.add("card-border-rad");
+  },
+
+  // support functions
+  reset: function () {
+    // remove card-border-rad from showPlayerCard
+    // remove hidden from showPlayerCard
+  },
 };
 
 game.init();
