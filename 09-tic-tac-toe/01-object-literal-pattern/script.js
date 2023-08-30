6ee11dba2f7e8a99d5605627d4e27a7738f1e7f8
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-30 15:46:22 +0200

    remove commented out code; cache players; bind showPlayerCard to each player (player1/2, computer); create showPlayerCard handler func; add comment to reset; create addRemoveHidden function

--- 09-tic-tac-toe/01-object-literal-pattern/script.js
+++ 09-tic-tac-toe/01-object-literal-pattern/script.js
@@ -10,33 +10,45 @@ const game = {
 
   cacheMenuDOM: function () {
     this.container = document.querySelector(".container");
-    this.player1Btn = this.container.querySelector(".player1-btn");
-    // this.player1Form = this.container.querySelector(".player1-form");
-
-    this.player2Btn = this.container.querySelector(".player2-btn");
-    // this.player2Form = this.container.querySelector(".player2-form");
+    this.players = document.querySelectorAll(".players");
   },
 
   bindEvents: function () {
-    this.player1Btn.addEventListener("click", this.showPlayerCard.bind(this));
-    this.player2Btn.addEventListener("click", this.showPlayerCard.bind(this));
+    this.players.forEach((player) => {
+      player.addEventListener("click", this.showPlayerCard.bind(this));
+    });
   },
 
   // handler functions
   showPlayerCard: function (event) {
-    if (event.target.classList.contains("player2-btn")) {
-      event.target.nextElementSibling.classList.add("hidden");
-      // event.target.parentElement.classList.add(".card-border-rad");
+    switch (event.target.classList[0]) {
+      case "player1-btn":
+        this.addRemoveHidden(event);
+        break;
+      case "player2-btn":
+        event.target.nextElementSibling.classList.add("hidden");
+        this.addRemoveHidden(event);
+        break;
+      case "computer-btn":
+        event.target.classList.add("selected");
+        event.target.classList.remove("hover");
+        break;
     }
-    event.target.classList.add("hidden");
-    event.target.parentElement.firstElementChild.classList.remove("hidden");
-    event.target.parentElement.classList.add("card-border-rad");
   },
 
   // support functions
   reset: function () {
     // remove card-border-rad from showPlayerCard
     // remove hidden from showPlayerCard
+    // add hover class to computer-btn
+    // reload the page to set it back to default
+  },
+
+  addRemoveHidden: function (event) {
+    event.target.classList.add("hidden");
+    event.target.parentElement.firstElementChild.classList.remove("hidden");
+    event.target.parentElement.classList.add("card-border-rad");
+    event.target.removeEventListener("click", this.showPlayerCard.bind(this));
   },
 };
 
