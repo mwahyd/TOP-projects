3ec1b37bd991f52916877fc8962ceb5a0fc7609e
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-08-31 13:01:54 +0200

    store player name and marker in playersInfo {}; set default values; set name and marker to both player1 and player2 based on user input; set default parameters to computer; refactor code

--- 09-tic-tac-toe/01-object-literal-pattern/script.js
+++ 09-tic-tac-toe/01-object-literal-pattern/script.js
@@ -2,20 +2,30 @@
 
 const game = {
   // root functions
+  playersInfo: {},
+
   init: function () {
     this.cacheMenuDOM();
     this.bindEvents();
+    this.setDefault();
     console.log("this works from init");
   },
 
   cacheMenuDOM: function () {
     this.container = document.querySelector(".container");
-    this.players = document.querySelectorAll(".players");
+    this.players = this.container.querySelectorAll(".players");
+    this.forms = this.container.querySelectorAll(".fields");
+    this.p1Input = this.container.querySelector("#first-player");
+    this.p2Input = this.container.querySelector("#second-player");
   },
 
   bindEvents: function () {
     this.players.forEach((player) => {
       player.addEventListener("click", this.showPlayerCard.bind(this));
+    });
+
+    this.forms.forEach((form) => {
+      form.addEventListener("click", this.getInputs.bind(this));
     });
   },
 
@@ -32,8 +42,46 @@ const game = {
       case "computer-btn":
         event.target.classList.add("selected");
         event.target.classList.remove("hover");
+        this.playersInfo["p2"] = "computer";
+        console.log(this.playersInfo);
         break;
     }
+  },
+
+  getInputs: function (event) {
+    if (event.target.nodeName !== "BUTTON") {
+      console.log("not a button click");
+      return;
+    }
+    switch (event.target.classList[0]) {
+      case "p1-markers":
+        this.p1Marker = this.selectMarker(event, "p1-marker1");
+        console.log(this.p1Marker);
+        break;
+      case "p2-markers":
+        this.p2Marker = this.selectMarker(event, "p2-marker1");
+        console.log(this.p2Marker);
+        break;
+      case "save-btn":
+        if (event.target.id === "save1") {
+          event.target.classList.add("saved");
+          this.p1Name = this.getName(this.p1Input);
+          this.playersInfo["p1"] = this.p1Name || "player 1";
+          this.playersInfo["p1Marker"] = this.p1Marker || "x";
+        } else if (event.target.id === "save2") {
+          event.target.classList.add("saved");
+          this.p2Name = this.getName(this.p2Input);
+          this.playersInfo["p2"] = this.p2Name || "player 2";
+          this.playersInfo["p2Marker"] = this.p2Marker || "o";
+        }
+        console.log(this.playersInfo);
+
+        break;
+    }
+
+    // console.log(this.playersInfo);
+    // console.log(event.target);
+    // console.log(event.target.classList.add("saved"));
   },
 
   // support functions
@@ -41,14 +89,42 @@ const game = {
     // remove card-border-rad from showPlayerCard
     // remove hidden from showPlayerCard
     // add hover class to computer-btn
-    // reload the page to set it back to default
+    // - reload the page to set it back to default
+    // remove saved from save buttons
+    // remove saved from marker buttons
   },
 
   addRemoveHidden: function (event) {
     event.target.classList.add("hidden");
     event.target.parentElement.firstElementChild.classList.remove("hidden");
     event.target.parentElement.classList.add("card-border-rad");
+    event.target.previousElementSibling.classList.add("fade-in");
     event.target.removeEventListener("click", this.showPlayerCard.bind(this));
+  },
+
+  setDefault: function () {
+    this.playersInfo["p1"] = "player 1";
+    this.playersInfo["p1Marker"] = "x";
+    this.playersInfo["p2"] = "player 2";
+    this.playersInfo["p2Marker"] = "o";
+    console.log(this.playersInfo);
+  },
+
+  selectMarker: function (event, buttonID) {
+    if (event.target.id === buttonID) {
+      event.target.classList.add("selected");
+      event.target.nextElementSibling.classList.remove("selected");
+    } else {
+      event.target.classList.add("selected");
+      event.target.previousElementSibling.classList.remove("selected");
+    }
+    return event.target.textContent.trim();
+  },
+
+  getName: function (playerInput) {
+    if (playerInput.value.trim() !== "") {
+      return playerInput.value.trim().toLowerCase();
+    }
   },
 };
 
