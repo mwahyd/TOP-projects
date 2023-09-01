2e63a853dad6bc7b4c283f09eefca52e299c7a1a
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-01 15:34:06 +0200

    create checkEmptySquare(); create placeMarker() which places a marker if square is empty; create setTurn() and flags (vars); based on the flag switch between player1 and player2; create gameController handler function(); based on turn (flag) allow the operations to execute

--- 09-tic-tac-toe/01-object-literal-pattern/script.js
+++ 09-tic-tac-toe/01-object-literal-pattern/script.js
@@ -4,6 +4,12 @@ const game = {
   // root functions
   playersInfo: {},
 
+  // flags
+  p1Turn: false,
+  p1MarkerPlaced: false,
+  p2Turn: false,
+  p2MarkerPlaced: false,
+
   init: function () {
     this.cacheDOM();
     this.bindEvents();
@@ -11,6 +17,7 @@ const game = {
 
     if (document.body.id === "game-page") {
       this.render();
+      // this.setTurn();
     }
   },
 
@@ -43,6 +50,10 @@ const game = {
     // game html events
     if (document.body.id === "game-page") {
       window.addEventListener("load", this.gameBoard.bind(this));
+      this.gameboardContainer.addEventListener(
+        "click",
+        this.gameController.bind(this)
+      );
     }
   },
 
@@ -50,35 +61,71 @@ const game = {
     this.playersInfo = JSON.parse(localStorage.getItem("playersInfo"))[0];
     console.log(this.playersInfo);
     // display all the player infomation
-    console.log(this.spans);
+    // console.log(this.spans);
     this.spans.forEach((item) => {
       switch (item.id) {
         case "timer":
-          console.log(item.id);
+          // console.log(item.id);
           item.textContent = "1:30";
           break;
         case "round":
-          console.log(item.id);
+          // console.log(item.id);
           item.textContent = 2;
           break;
         case "player1-name":
-          console.log(item.id);
+          // console.log(item.id);
           item.textContent = this.playersInfo["p1"];
           break;
         case "p1-marker":
-          console.log(item.id);
-          item.textContent = this.playersInfo["p1Marker"];
+          // console.log(item.id);
+          item.textContent = this.playersInfo["p1M"];
           break;
         case "player2-name":
-          console.log(item.id);
+          // console.log(item.id);
           item.textContent = this.playersInfo["p2"];
           break;
         case "p2-marker":
-          console.log(item.id);
-          item.textContent = this.playersInfo["p2Marker"];
+          // console.log(item.id);
+          item.textContent = this.playersInfo["p2M"];
           break;
       }
     });
+  },
+
+  setTurn: function () {
+    console.log("p1 turn", this.p1Turn, "p2 turn", this.p2Turn);
+    if (this.p1Turn === false && this.p2Turn === false) {
+      this.p1Turn = true;
+      return "p1";
+    } else if (this.p1Turn === true && this.p1MarkerPlaced === true) {
+      console.log("player1 has played turn");
+      this.p1Turn = false;
+      this.p1MarkerPlaced = false;
+      this.p2Turn = true;
+      return "p2";
+    } else if (this.p2Turn === true && this.p2MarkerPlaced === true) {
+      console.log("player2 has played turn");
+      this.p2Turn = false;
+      this.p2MarkerPlaced = false;
+      this.p1Turn = true;
+      return "p1";
+    }
+  },
+
+  placeMarker: function (event, marker) {
+    // check which player's turn
+    console.log(event.target);
+    // check if squre is empty
+    console.log("empty square", this.checkEmptySquare(event.target));
+    // place marker
+    if (this.checkEmptySquare(event.target)) {
+      event.target.textContent = marker;
+    } else {
+      console.log("square not empty");
+    }
+
+    // call change turn function
+    this.setTurn();
   },
 
   // handler functions
@@ -130,7 +177,7 @@ const game = {
   },
 
   gameBoard: function () {
-    this.gameboard = ["x", "o", "x", "o", "x", "", "", "o", "x"];
+    this.gameboard = ["", "", "", "", "", "", "", "", "x"];
     this.gameboard.forEach((marker, index) => {
       const div = document.createElement("div");
       div.setAttribute("data-index", index);
@@ -142,6 +189,32 @@ const game = {
     console.log(this.gameboard);
   },
 
+  gameController: function (event) {
+    // check player turn,
+    this.isTurn = this.setTurn();
+    console.log(this.isTurn);
+    // get player marker,
+    switch (this.isTurn) {
+      case "p1":
+        this.markerP1 = this.playersInfo[`${this.isTurn}M`];
+        console.log(this.markerP1);
+        this.placeMarker(event, this.markerP1);
+        this.p1MarkerPlaced = true;
+        console.log("p1MarkerPlaced", this.p1MarkerPlaced);
+        break;
+
+      case "p2":
+        this.markerP2 = this.playersInfo[`${this.isTurn}M`];
+        console.log(this.markerP2);
+        this.placeMarker(event, this.markerP2);
+        this.p2MarkerPlaced = true;
+        console.log("p2MarkerPlaced", this.p2MarkerPlaced);
+        break;
+    }
+    // allow player to place marker if square is empty,
+    // transfer turn to player2
+  },
+
   // support functions
   reset: function () {
     // remove card-border-rad from showPlayerCard
@@ -151,6 +224,8 @@ const game = {
     // remove saved from save buttons
     // remove saved from marker buttons
     // ! clear local storage FOR EVERY NEW GAME
+    // clear timer for every game
+    //
   },
 
   addRemoveHidden: function (event) {
@@ -194,6 +269,10 @@ const game = {
 
   storePlayerInfo: function (array) {
     localStorage.setItem("playersInfo", JSON.stringify(array));
+  },
+
+  checkEmptySquare: function (target) {
+    return target.textContent === "" ? true : false;
   },
 };
 
