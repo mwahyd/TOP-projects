bbfd37337468fb8458001e8a553b93ea3052d050
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-12 15:38:45 +0200

    add image loaders

--- 10-restaurant-page/webpack.config.js
+++ 10-restaurant-page/webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(svg|gif|jpg|jpeg|png)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
