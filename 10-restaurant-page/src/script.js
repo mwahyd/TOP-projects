ce164e514c8a9616567615375784a16eb9fe0c4e
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-09-12 14:44:31 +0200

    dynamically add HTML to the document; create _render() to display the elements on DOM

--- 10-restaurant-page/src/script.js
+++ 10-restaurant-page/src/script.js
@@ -4,20 +4,62 @@ import style from "./style.css";
 const restaurantPage = (function () {
   // cache DOM
   const doc = document.querySelector("body");
+
+  // create default elements
+  const header = document.createElement("header");
+  const main = document.createElement("main");
+  const footer = document.createElement("footer");
+  const container = document.createElement("div");
+
   _render();
 
   // root functions
   function _render() {
-    doc.appendChild(_createContent());
+    // container
+    container.id = "container";
+    container.appendChild(header);
+    container.appendChild(main);
+    container.appendChild(footer);
+
+    doc.appendChild(container);
+
+    // header
+    // title
+    const h1 = _createElement("h1");
+    h1.id = "title";
+    h1.textContent = "Bakers!";
+    header.appendChild(h1);
+
+    // nav
+    const nav = _createElement("nav");
+    const home = _createElement("li");
+    const menu = _createElement("li");
+    const contact = _createElement("li");
+
+    nav.id = "nav";
+    home.id = "home";
+    menu.id = "menu";
+    contact.id = "contact";
+    home.textContent = "home";
+    menu.textContent = "menu";
+    contact.textContent = "contact";
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+
+    header.appendChild(nav);
+
+    // main
+    main.textContent = "goats";
+
+    // footer
+    footer.textContent = "nah nah";
   }
 
   // handler functions
 
   // support functions
-  function _createContent() {
-    const content = document.createElement("div");
-    content.id = "content";
-    content.innerHTML = "HELLLOOOOO";
-    return content;
+  function _createElement(name) {
+    return document.createElement(name);
   }
 })();
