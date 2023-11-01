5ecb8e43931ffa2f9e9543738e2cface9fec3dff
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-01 17:54:52 +0100

    refactor cacheDOM; add tab ignored functinality; add regex test for email

--- 12-form-validation/project-form-validation/script.js
+++ 12-form-validation/project-form-validation/script.js
@@ -5,24 +5,43 @@ const validation = {
   },
 
   cacheDOM: function () {
+    this.form = document.querySelector("#form");
+    this.errorDiv = this.form.previousElementSibling;
+    this.postFields = this.form.querySelectorAll("[data-id]");
+
     return [
-      (this.form = document.querySelector("#form")),
-      (this.errorDiv = this.form.previousElementSibling),
       (this.emailField = this.form.querySelector("#email")),
       (this.passField = this.form.querySelector("#password")),
       (this.conPassField = this.form.querySelector("#conf-password")),
       (this.countryField = this.form.querySelector("#country")),
-      (this.postFields = this.form.querySelectorAll("[data-id]")),
     ];
   },
 
   listeners: function () {
+    // if user tabs through and ignores fields
+    validation.cacheDOM().forEach((field) => {
+      field.addEventListener("blur", validation.error);
+    });
+
     // check field after change event
     this.emailField.addEventListener("change", validation.testEmail);
   },
 
   testEmail: function (event) {
-    console.log("hello");
+    const regex =
+      /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
+    if (regex.test(event.target.value)) {
+      console.log("pass");
+    } else {
+      console.log("fail");
+    }
+  },
+
+  error: function (event) {
+    // if field empty and blurred
+    if (event.type === "blur" && event.target.value === "") {
+      event.target.classList.add("error-highlight");
+    }
   },
 };
 
