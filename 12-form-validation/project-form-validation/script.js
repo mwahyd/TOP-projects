fbdf05aede17e43ac86fa5e8969ecbbb1a421d76
mwahyd <72066821+mwahyd@users.noreply.github.com>
2023-11-02 15:05:58 +0100

    add special error messages for password; refactor error() to check for specific errorTypes; refactor valid() to check for solved errorTypes; create tests for password

--- 12-form-validation/project-form-validation/script.js
+++ 12-form-validation/project-form-validation/script.js
@@ -21,9 +21,14 @@ const validation = {
   render: function () {
     this.errorDiv.innerHTML = "";
     this.errorCodes.forEach((errorCode) => {
-      const [field, code] = errorCode.split(/(?=[A-Z])/);
       const p = document.createElement("p");
       p.id = errorCode;
+      if (errorCode === "passwordPassLength") {
+        p.textContent = `\u2757 password shorter than 8 characters`;
+        this.errorDiv.appendChild(p);
+        return;
+      }
+      const [field, code] = errorCode.split(/(?=[A-Z])/);
       p.textContent = `\u2757${field} field is ${code}`;
       this.errorDiv.appendChild(p);
     });
@@ -40,16 +45,25 @@ const validation = {
 
     // check for change and test field
     this.emailField.addEventListener("change", this.testEmail.bind(this));
+    this.passField.addEventListener("change", this.testPassword.bind(this));
   },
 
-  error: function (event) {
+  error: function (event, errorType) {
     // if field empty and blurred
     if (event.type === "blur" && event.target.value === "") {
       event.target.classList.add("empty-highlight");
       this.raiseError(event.target, "Empty");
     } else if (event.type === "change") {
-      event.target.classList.add("error-highlight");
-      this.raiseError(event.target, "Invalid");
+      switch (errorType) {
+        case "emailError":
+          event.target.classList.add("error-highlight");
+          this.raiseError(event.target, "Invalid");
+          break;
+        case "passLength":
+          console.log("pass length error");
+          event.target.classList.add("error-highlight");
+          this.raiseError(event.target, "PassLength");
+      }
     }
   },
 
@@ -61,16 +75,21 @@ const validation = {
     this.render();
   },
 
-  valid: function (event) {
+  valid: function (event, errorType) {
     // if field contains data
     if (event.type === "blur" && event.target.value !== "") {
-      // - creates bug which does not let error highlight to be added when change event
       event.target.classList.remove("empty-highlight");
       this.removeError(event.target, "Empty");
     } else if (event.type === "change") {
-      event.target.classList.remove("error-highlight");
-      this.removeError(event.target, "Invalid");
-      console.log(event.target);
+      switch (errorType) {
+        case "emailError":
+          event.target.classList.remove("error-highlight");
+          this.removeError(event.target, "Invalid");
+          break;
+        case "passLength":
+          event.target.classList.remove("error-highlight");
+          this.removeError(event.target, "PassLength");
+      }
     }
   },
 
@@ -88,9 +107,21 @@ const validation = {
     const regex =
       /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
     if (regex.test(event.target.value)) {
-      this.valid(event);
+      this.valid(event, "emailError");
     } else {
-      this.error(event);
+      this.error(event, "emailError");
+    }
+  },
+
+  testPassword: function (event) {
+    const uCase = /[A-Z]{1,}/;
+    const lCase = /[a-z]/;
+    const num = /[0-9]{1,}/;
+    const special = /[\W]{1,}/;
+    if (event.target.value.length < 8) {
+      this.error(event, "passLength");
+    } else {
+      this.valid(event, "passLength");
     }
   },
 };
