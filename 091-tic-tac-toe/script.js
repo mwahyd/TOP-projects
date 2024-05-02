6bccdc8fcc566a15068862ce2960e37ceb7ea795
mwahyd <72066821+mwahyd@users.noreply.github.com>
2024-05-02 16:04:25 +0100

    enable the start button when condition met

--- 091-tic-tac-toe/script.js
+++ 091-tic-tac-toe/script.js
@@ -5,26 +5,30 @@ const menu = (function () {
   const container = document.querySelector("#container");
   const players = container.querySelector(".players");
   const playerBtns = players.querySelectorAll("button");
+  const startBtn = container.querySelector("#start");
   // bind events
-  playerBtns.forEach((btn) => btn.addEventListener("click", _selectPlayer));
+  playerBtns.forEach((btn) => btn.addEventListener("click", selectPlayer));
+  startBtn.addEventListener("click", checkGameParameters);
   // render
   // handler functions
-  function _selectPlayer(event) {
+  function selectPlayer(event) {
     switch (event.target.id) {
       case "p1-btn":
-        _selectPlayer1(event.target);
+        selectPlayer1(event.target);
         break;
       case "p2-btn":
-        _selectPlayer2(event.target);
+        disableStartButton();
+        selectPlayer2(event.target);
         break;
       case "comp-btn":
-        // _selectComputer(event.target);
-        _addClassList(event.target, "selected");
-        _removeClassList(event.target, "hover");
+        // selectComputer(event.target);
+        addClassList(event.target, "selected");
+        removeClassList(event.target, "hover");
+        isReady();
         break;
     }
   }
-  function _closeForm(event) {
+  function closeForm(event) {
     event.preventDefault();
     const form = players.querySelector("#p2 .form");
     const p2Btn = players.querySelector("#p2-btn");
@@ -32,88 +36,111 @@ const menu = (function () {
     const selectedMarker = players.querySelector(`.marker.p2.selected`);
     const p2Save = players.querySelector("#p2 .saved");
     players.querySelector("#p2 input").value = "";
-    _addClassList(form, "hidden");
-    _removeClassList(p2Btn, "hidden");
-    _removeClassList(compBtn, "hidden");
-    p2Save && _removeClassList(p2Save, "saved");
-    selectedMarker && _removeClassList(selectedMarker, "selected");
+    addClassList(form, "hidden");
+    removeClassList(p2Btn, "hidden");
+    removeClassList(compBtn, "hidden");
+    p2Save && removeClassList(p2Save, "saved");
+    selectedMarker && removeClassList(selectedMarker, "selected");
     playerInfo["p2"] = undefined;
     playerInfo["p2M"] = undefined;
+    disableStartButton();
   }
-  function _getData(event) {
+  function getData(event) {
     event.preventDefault();
     const id = event.target.classList[1];
     // let marker;
     switch (event.target.classList[0]) {
       case "marker":
-        _selectMarker(event.target, id);
+        selectMarker(event.target, id);
         break;
       case "save-btn":
-        _addClassList(event.target, "saved");
-        _saveInfo(id);
+        addClassList(event.target, "saved");
+        saveInfo(id);
         break;
     }
     // console.log(marker);
   }
-  // support functions
-  function _selectPlayer1(button) {
-    const form = players.querySelector(`#p1 form`);
-    _displayForm(button, form, "p1");
-    form.addEventListener("click", _getData);
+  function checkGameParameters(event) {
+    console.log(event.target);
   }
-  function _selectPlayer2(button) {
+  //  support functions
+  function selectPlayer1(button) {
+    const form = players.querySelector(`#p1 form`);
+    displayForm(button, form, "p1");
+    form.addEventListener("click", getData);
+  }
+  function selectPlayer2(button) {
     const form = players.querySelector(`#p2 form`);
     const computer = players.querySelector("#comp-btn");
     if (computer) {
-      _addClassList(computer, "hover");
-      _removeClassList(computer, "selected");
+      addClassList(computer, "hover");
+      removeClassList(computer, "selected");
     }
-    _displayForm(button, form, "p2");
+    displayForm(button, form, "p2");
     const formClose = players.querySelector("#p2 .form .close");
-    formClose.addEventListener("click", _closeForm);
-    form.addEventListener("click", _getData);
+    formClose.addEventListener("click", closeForm);
+    form.addEventListener("click", getData);
   }
-  function _displayForm(button, form, playerID) {
-    _removeClassList(form, "hidden");
-    _addClassList(button, "hidden");
+  function displayForm(button, form, playerID) {
+    removeClassList(form, "hidden");
+    addClassList(button, "hidden");
     if (playerID === "p2") {
-      _addClassList(button.nextElementSibling, "hidden");
+      addClassList(button.nextElementSibling, "hidden");
     }
   }
-  function _selectMarker(button, id) {
+  function selectMarker(button, id) {
     const selectedMarker = players.querySelector(`.marker.${id}.selected`);
-    selectedMarker && _removeClassList(selectedMarker, "selected");
-    button.classList[1] === id && _addClassList(button, "selected");
+    selectedMarker && removeClassList(selectedMarker, "selected");
+    button.classList[1] === id && addClassList(button, "selected");
   }
-  function _getName(id) {
+  function getName(id) {
     const input = players.querySelector(`#${id} input`);
     if (input.value.trim() !== "") return input.value.trim().toLowerCase();
   }
-  function _getMarker(id) {
+  function getMarker(id) {
     if (players.querySelector(`.marker.${id}.selected`)) {
       return players.querySelector(`.marker.${id}.selected`).textContent.trim();
     }
   }
-  function _saveInfo(id) {
+  function saveInfo(id) {
     if (id === "p1") {
-      playerInfo["p1"] = _getName(id) || id;
-      playerInfo["p1M"] = _getMarker(id) || "x";
+      playerInfo["p1"] = getName(id) || id;
+      playerInfo["p1M"] = getMarker(id) || "x";
     } else {
-      playerInfo["p2"] = _getName(id) || id;
-      playerInfo["p2M"] = _getMarker(id) || "o";
+      playerInfo["p2"] = getName(id) || id;
+      playerInfo["p2M"] = getMarker(id) || "o";
     }
     console.log(playerInfo);
+    isReady();
   }
-  function _addClassList(element, ...args) {
+  function isReady() {
+    // check if p1 save clicked
+    // check if p2 save clicked OR computer selected
+    const p1Saved = players.querySelector("#p1 .saved");
+    const p2Saved = players.querySelector("#p2 .saved");
+    const compSelected = players.querySelector("#comp-btn.selected");
+    // console.log(p1Saved, p2Saved, compSelected);
+    if (p1Saved && (p2Saved || compSelected)) {
+      enableStartButton();
+    }
+  }
+  function enableStartButton() {
+    // console.log(startBtn);
+    startBtn.removeAttribute("disabled");
+  }
+  function disableStartButton() {
+    startBtn.disabled = true;
+  }
+  function addClassList(element, ...args) {
     for (const arg of args) {
       element.classList.add(arg);
     }
   }
-  function _removeClassList(element, ...args) {
+  function removeClassList(element, ...args) {
     for (const arg of args) {
       element.classList.remove(arg);
     }
   }
   //? when game starts, remove event listeners from menu page
-  function _removeListener(element, event) {}
+  function removeListener(element, event) {}
 })();
