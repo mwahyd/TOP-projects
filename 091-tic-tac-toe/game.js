22425636bc53f58262dabfebfff15b00d5d3c342
mwahyd <72066821+mwahyd@users.noreply.github.com>
2024-06-05 13:02:48 +0100

    display player info on game header

--- 091-tic-tac-toe/game.js
+++ 091-tic-tac-toe/game.js
@@ -13,6 +13,7 @@ const gameboard = (function () {
     // draw the board on the screen
     for (let i = 0; i < board.length; i++) {
       const div = document.createElement("div");
+      div.setAttribute("data-index", i);
       gameboardDiv.append(div);
     }
   }
@@ -27,20 +28,39 @@ const game = (function () {
   // initialise
   console.log(document.body.id);
   if (document.body.id !== "game-page") return;
+  let playersInfo = {};
   // cache DOM
   const container = document.querySelector("#container");
   // bind events
   // render
   render();
   function render() {
+    const content = container.querySelector("#content");
+    displayBoard(content);
+    getStoredData();
+    displayGameHeader();
+  }
+  function displayBoard(div) {
     // display the screen
     // animate it to appear from top to bottom
-    const content = container.querySelector("#content");
-    Tools.removeClassList(content, "hidden");
-    Tools.addClassList(content, "slide-down");
+    Tools.removeClassList(div, "hidden");
+    Tools.addClassList(div, "slide-down");
   }
-
-  console.log(content);
-
-  // get info from local storage
+  function getStoredData() {
+    // get info from local storage
+    playersInfo = Tools.getFromLocalStorage("playersInfo");
+    console.log(playersInfo);
+  }
+  function displayGameHeader() {
+    const round = container.querySelector("#round");
+    const p1Name = container.querySelector("#p1-name");
+    const p1Marker = container.querySelector("#p1-marker");
+    const p2Name = container.querySelector("#p2-name");
+    const p2Marker = container.querySelector("#p2-marker");
+    round.textContent = 1;
+    p1Name.textContent = `${playersInfo["p1"]}:`;
+    p1Marker.textContent = `${playersInfo["p1M"]}`;
+    p2Name.textContent = `${playersInfo["p2"]}:`;
+    p2Marker.textContent = `${playersInfo["p2M"]}`;
+  }
 })();
