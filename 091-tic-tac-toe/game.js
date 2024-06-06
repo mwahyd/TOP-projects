e03e2f6c0fcfdf8604403f7a2cef9a067713e070
mwahyd <72066821+mwahyd@users.noreply.github.com>
2024-06-06 17:25:42 +0100

    refactor code; create gameboard API functions; create function to display screen; allow render method to run after screen loaded; animate game header and gameboard

--- 091-tic-tac-toe/game.js
+++ 091-tic-tac-toe/game.js
@@ -3,25 +3,13 @@ import Tools from "./tools.js";
 const gameboard = (function () {
   // initialise
   const board = ["", "", "", "", "", "", "", "", ""];
-  // cache DOM
-  const container = document.querySelector("#container");
-  // bind events
-  // render
-  render();
-  function render() {
-    const gameboardDiv = container.querySelector("#gameboard");
-    // draw the board on the screen
-    for (let i = 0; i < board.length; i++) {
-      const div = document.createElement("div");
-      div.setAttribute("data-index", i);
-      gameboardDiv.append(div);
-    }
-  }
-
-  console.log(board);
   // APIs
-  // get board function
-  // update board function
+  function getboard() {
+    return board;
+  }
+  function updateBoard(index, marker) {}
+  function resetBoard() {}
+  return { getboard, updateBoard, resetBoard };
 })();
 
 const game = (function () {
@@ -30,37 +18,66 @@ const game = (function () {
   if (document.body.id !== "game-page") return;
   let playersInfo = {};
   // cache DOM
-  const container = document.querySelector("#container");
+  const container = document.querySelector("#app-container");
+  const content = container.querySelector("#app-content");
+
   // bind events
-  // render
-  render();
-  function render() {
-    const content = container.querySelector("#content");
-    displayBoard(content);
-    getStoredData();
-    displayGameHeader();
-  }
-  function displayBoard(div) {
+  content.addEventListener("animationend", render, { once: true });
+
+  // display the screen
+  const displayScreen = (function () {
     // display the screen
     // animate it to appear from top to bottom
-    Tools.removeClassList(div, "hidden");
-    Tools.addClassList(div, "slide-down");
+    Tools.removeClassList(content, "hidden");
+    Tools.addClassList(content, "slide-down");
+  })();
+
+  // render
+  function render() {
+    console.log("animation ended");
+    // display the game header
+    drawHeader();
+    // display the gameboard
+    drawBoard();
+    // populate header
+    populateHeader();
+  }
+  function drawBoard() {
+    const gameboardDiv = container.querySelector("#app-gameboard");
+    const boardArray = gameboard.getboard();
+    for (let i = 0; i < boardArray.length; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("data-index", i);
+      Tools.addClassList(div, "square", "highlight");
+      gameboardDiv.append(div);
+    }
+    Tools.addClassList(gameboardDiv, "reveal");
+  }
+  function drawHeader() {
+    const header = container.querySelector("#game-header");
+    Tools.removeClassList(header, "hidden");
+    Tools.addClassList(header, "reveal");
+  }
+  function populateHeader() {
+    getStoredData();
+    const headerElements = getHeaderElements();
+    headerElements.round.textContent = 1;
+    headerElements.p1Name.textContent = `${playersInfo["p1"]}:`;
+    headerElements.p1Marker.textContent = `${playersInfo["p1M"]}`;
+    headerElements.p2Name.textContent = `${playersInfo["p2"]}:`;
+    headerElements.p2Marker.textContent = `${playersInfo["p2M"]}`;
   }
   function getStoredData() {
     // get info from local storage
     playersInfo = Tools.getFromLocalStorage("playersInfo");
     console.log(playersInfo);
   }
-  function displayGameHeader() {
-    const round = container.querySelector("#round");
+  function getHeaderElements() {
+    const round = container.querySelector("#app-round");
     const p1Name = container.querySelector("#p1-name");
     const p1Marker = container.querySelector("#p1-marker");
     const p2Name = container.querySelector("#p2-name");
     const p2Marker = container.querySelector("#p2-marker");
-    round.textContent = 1;
-    p1Name.textContent = `${playersInfo["p1"]}:`;
-    p1Marker.textContent = `${playersInfo["p1M"]}`;
-    p2Name.textContent = `${playersInfo["p2"]}:`;
-    p2Marker.textContent = `${playersInfo["p2M"]}`;
+    return { round, p1Name, p1Marker, p2Name, p2Marker };
   }
 })();
