dbb2d612b7174416d33e65488317a24de389b547
mwahyd <72066821+mwahyd@users.noreply.github.com>
2024-06-20 16:06:31 +0100

    add the logic to select difficulty for CPU

--- 091-tic-tac-toe/script.js
+++ 091-tic-tac-toe/script.js
@@ -2,13 +2,16 @@ import Tools from "./tools.js";
 const menu = (function () {
   // initialise
   const playerInfo = {};
+  let difficulties = [];
   // cache DOM
   const container = document.querySelector("#app-container");
   const players = container.querySelector(".players");
   const playerBtns = players.querySelectorAll("button");
+  const difficulty = players.querySelector("#set-difficulty");
   const startBtn = container.querySelector("#start");
   // bind events
   playerBtns.forEach((btn) => btn.addEventListener("click", selectPlayer));
+  difficulty.addEventListener("click", setGameDifficulty);
   startBtn.addEventListener("click", saveParaStartGame);
   // handler functions
   function selectPlayer(event) {
@@ -28,6 +31,17 @@ const menu = (function () {
         break;
     }
   }
+  function setGameDifficulty(event) {
+    if (event.target.nodeName !== "BUTTON") return;
+    const prevSelectedBtn = difficulty.querySelector("button.select");
+    prevSelectedBtn && Tools.removeClassList(prevSelectedBtn, "select");
+    const button = event.target;
+    difficulties = [];
+    Tools.addClassList(button, "select");
+    difficulties.push(event.target.id);
+    Object.assign(playerInfo, { difficulty: difficulties[0] });
+    isReady();
+  }
   function closeForm(event) {
     event.preventDefault();
     const form = players.querySelector("#p2 .form");
@@ -40,8 +54,10 @@ const menu = (function () {
     Tools.removeClassList(form, "reveal");
     Tools.addClassList(p2Btn, "reveal");
     Tools.addClassList(compBtn, "reveal");
+    Tools.addClassList(difficulty, "reveal");
     Tools.removeClassList(p2Btn, "hidden");
     Tools.removeClassList(compBtn, "hidden");
+    Tools.removeClassList(difficulty, "hidden");
     p2Save && Tools.removeClassList(p2Save, "saved");
     selectedMarker && Tools.removeClassList(selectedMarker, "selected");
     playerInfo["p2"] = undefined;
@@ -81,32 +97,40 @@ const menu = (function () {
   //  support functions
   function selectPlayer1(button) {
     const form = players.querySelector(`#p1 form`);
-    displayForm(button, form, "p1");
+    displayFormResetCPU(button, form, "p1");
     form.addEventListener("click", getData);
   }
   function selectPlayer2(button) {
     const form = players.querySelector(`#p2 form`);
     const computer = players.querySelector("#comp-btn");
+    const difficulty = players.querySelector("#set-difficulty > button.select");
     if (computer) {
       Tools.addClassList(computer, "hover");
       Tools.removeClassList(computer, "selected");
     }
-    displayForm(button, form, "p2");
+    if (difficulty) {
+      Tools.removeClassList(difficulty, "select");
+    }
+    displayFormResetCPU(button, form, "p2");
     const formClose = players.querySelector("#p2 .form .close");
     formClose.addEventListener("click", closeForm);
     form.addEventListener("click", getData);
   }
   function setP2Computer() {
-    !playerInfo["p2"] && Object.assign(playerInfo, { p2: "COMP", p2M: "o" });
+    !playerInfo["p2"] && Object.assign(playerInfo, { p2: "CPU", p2M: "o" });
   }
-  function displayForm(button, form, playerID) {
+  function displayFormResetCPU(button, form, playerID) {
     Tools.removeClassList(form, "hidden");
     Tools.removeClassList(button, "reveal");
     Tools.addClassList(form, "reveal");
     Tools.addClassList(button, "hidden");
     if (playerID === "p2") {
+      // hide Computer and Difficulty buttons
+      Tools.addClassList(difficulty, "hidden");
       Tools.addClassList(button.nextElementSibling, "hidden");
       Tools.removeClassList(button.nextElementSibling, "reveal");
+      difficulties = [];
+      delete playerInfo["difficulty"];
     }
   }
   function selectMarker(button, id) {
@@ -131,14 +155,18 @@ const menu = (function () {
       playerInfo["p2"] = getName(id) || id;
       playerInfo["p2M"] = getMarker(id) || "o";
     }
+    console.log(playerInfo);
     isReady();
   }
   function isReady() {
     const p1Saved = players.querySelector("#p1 .saved");
     const p2Saved = players.querySelector("#p2 .saved");
     const compSelected = players.querySelector("#comp-btn.selected");
-    if (p1Saved && (p2Saved || compSelected)) {
+    const difficulty = players.querySelector("#set-difficulty > button.select");
+    if (p1Saved && (p2Saved || (compSelected && difficulty))) {
       enableStartButton();
+      console.log(difficulties);
+      console.log(playerInfo);
     }
   }
   function enableStartButton() {
