9799d6d5b6b8e4c460102a19dac0f70ed8bbf897
mwahyd <72066821+mwahyd@users.noreply.github.com>
2024-08-28 11:07:42 +0100

    create logic to validate password field input when user blurs from field or types in the field

--- 12-login-signup/signup-script.js
+++ 12-login-signup/signup-script.js
@@ -1,10 +1,5 @@
 (function formConstraints() {
   // initialise
-  const errorMap = {
-    // key === id; value === data-attribute
-    name: "[name-error]",
-    surname: "[surname-error]",
-  };
   // cache dom
   const doc = document.querySelector("[section-signup]");
   const nameFields = doc.querySelectorAll("input#name, input#surname");
@@ -14,21 +9,32 @@
 
   // bind events
   nameFields.forEach((nameField) => {
-    nameField.addEventListener("blur", handleFocusBlur);
+    nameField.addEventListener("blur", handleNameFocusBlur);
   });
+  emailField.addEventListener("blur", emailConstraint);
   emailField.addEventListener("input", emailConstraint);
   showPassBtn.addEventListener("click", showPassword);
+  // passField.addEventListener("blur", handlePassFocusBlur);
+  // passField.addEventListener("input", handlePassFocusBlur);
+
+  passField.addEventListener("blur", handlePassBlur);
+  passField.addEventListener("input", handlePassInput);
 
   // handler functions
 
   // * name functions
-  function handleFocusBlur(ev) {
+  function handleNameFocusBlur(ev) {
+    const errorMap = {
+      // key === id; value === data-attribute
+      name: "[name-error]",
+      surname: "[surname-error]",
+    };
     const fieldId = ev.target.id;
     const errorSpanSelector = errorMap[fieldId];
-    focusConstraint(ev, errorSpanSelector);
+    nameConstraint(ev, errorSpanSelector);
   }
 
-  function focusConstraint(ev, spanName) {
+  function nameConstraint(ev, spanName) {
     // focused and blured AND EMPTY flag error!
     const nameField = ev.target;
     const errorSpan = doc.querySelector(spanName);
@@ -47,8 +53,10 @@
   // * email functions
   function emailConstraint(ev) {
     const errorSpan = doc.querySelector("[email-error]");
-    if (ev.target.value === "") {
-      removeError(errorSpan);
+    const emailField = ev.target;
+    if (document.activeElement !== emailField && emailField.value.trim() === "") {
+      resetElement(emailField);
+      raiseError(errorSpan, "remove", "hidden", " Field cannot be blank");
     } else if (!verifyEmail(ev.target.value)) {
       const msg = ` Invalid email format [abc@abc.xyz]`;
       raiseError(errorSpan, "remove", "hidden", msg);
@@ -71,11 +79,69 @@
       : (passField.type = "password");
   }
 
+  // handle blur event (when user leaves password field)
+  function handlePassBlur(ev) {
+    const passReqsSpans = getPassRequirements();
+    if (ev.target.value.trim() === "") {
+      highlightRequirementSpans(passReqsSpans, true);
+    }
+  }
+
+  // handle input event (when user types in password field)
+  function handlePassInput(ev) {
+    const passResults = verifyPassword(ev.target.value);
+    validatePassword(passResults);
+  }
+
+  // Retrieve all password requirements elements
+  function getPassRequirements() {
+    return Array.from(doc.querySelectorAll("[pass-req]"));
+  }
+
+  // Highlight requirements based on validity
+  function highlightRequirementSpans(reqSpans, isInvalid) {
+    reqSpans.forEach((span) => {
+      if (isInvalid) {
+        raiseError(span, "add", "invalid");
+      }
+    });
+  }
+
+  // Retrieve a single password requirement element by key
+  function getPassReqSpanUsingKey(key) {
+    key = key.slice(3).toLowerCase();
+    return doc.querySelector(`[class*=${key}]`);
+  }
+
+  // test password against the requirements using regex
+  function verifyPassword(password) {
+    return {
+      hasLetter: /[a-zA-Z]/.test(password),
+      hasNum: /\d/.test(password),
+      hasChar: /[!@#$%^&*(),.?":{}|<>]/.test(password),
+      hasLength: password.length >= 8,
+    };
+  }
+
+  function validatePassword(testedPassObj) {
+    Object.entries(testedPassObj).forEach(([key, isValid]) => {
+      const reqSpan = getPassReqSpanUsingKey(key);
+      updateReqSpanIcon(reqSpan, isValid);
+    });
+  }
+
+  // Update the icon of a specific requirement based on validity
+  function updateReqSpanIcon(reqSpan, isValid) {
+    const icon = reqSpan.querySelector(".req__icon");
+    icon.textContent = isValid ? "\u26AB\uFE0E" : "\u25CB\uFE0E"; // Filled or open circle
+    isValid ? icon.setAttribute("valid", "") : icon.removeAttribute("valid");
+  }
+
   // * error functions
-  function raiseError(element, toggle, className, errorMsg) {
+  function raiseError(element, toggle, className, errorMsg = false) {
+    if (errorMsg) element.textContent = errorMsg;
     if (toggle === "add") element.classList.add(className);
     else if (toggle === "remove") element.classList.remove(className);
-    element.textContent = errorMsg;
   }
 
   function removeError(element, toggle, className, text = false) {
